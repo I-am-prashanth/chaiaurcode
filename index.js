49d9bbca83946a81fe4611095e08fe9ec21a6400
I-am-prashanth <prashanthpathigari@gmail.com>
2025-01-26 21:23:14 +0530

    first commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app=express();
 app.get("/",(req,res)=>{
     res.send('<h1>prashanth</h1>')
 })
-app.listen(5000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("posrt is running")
 })
 
